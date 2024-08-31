bb054eccda272afdfc91c2904b088f20cc399123
Carlos <41452910+usarral@users.noreply.github.com>
2024-08-31 17:47:03 +0200

    refactor: Generate theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [require("@tailwindcss/typography"),require("daisyui")],
-	daisyui: {
-		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-		darkTheme: "dark", // name of one of the included themes for dark mode
-		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
-	  }
+  themes: [
+    {
+      usarral: {
+
+        "primary": "#e5e7eb",
+
+        "secondary": "#ffcb23",
+
+        "accent": "#3b82f6",
+
+        "neutral": "#06060f",
+
+        "base-100": "#24281e",
+
+        "info": "#00a6f2",
+
+        "success": "#00df77",
+
+        "warning": "#ff9600",
+
+        "error": "#f40047",
+      },
+    },
+  ],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        bluegray: '#20252c'
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
+  }
 }
