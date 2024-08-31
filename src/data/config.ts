6d5fab021f3894b6cc1b495098d2a63cbddf3912
Carlos <41452910+usarral@users.noreply.github.com>
2024-08-31 18:06:10 +0200

    refactor: Manage menu using config.ts

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,7 +1,41 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
+import type MenuItem from 'src/lib/interfaces/MenuItem';
+import type Project from '../lib/interfaces/Project'
+
 
 export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
 export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
+
+export const MENU_ITEMS: MenuItem[] = [
+  {
+    Page: 'Home',
+    Link: '/',
+    id: 'home'
+  },
+  {
+    Page: 'Projects',
+    Link: '/projects',
+    id: 'projects'
+  },
+  {
+    Page: 'CV',
+    Link: '/cv',
+    id: 'cv'
+  },
+  {
+    Page: 'Contact',
+    Link: '/contact',
+    id: 'contact'
+  }
+]
+
+export const PROJECTS: Project[] = [
+
+
+
+
+]
+
---
+++ src/lib/interfaces/MenuItem.ts
@@ -0,0 +1,5 @@
+export default interface menuItem {
+  Page: String,
+  Link: String,
+  id: String
+}
