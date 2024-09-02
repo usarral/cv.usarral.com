5a1b18ce50adac5e771fa668e9df4dbdb3789c8e
Carlos <41452910+usarral@users.noreply.github.com>
2024-09-02 23:16:11 +0200

    feat: new CV page

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,7 +1,5 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-import type MenuItem from "src/lib/interfaces/MenuItem";
-import type Project from "../lib/interfaces/Project";
+import type MenuItem from "@lib/interfaces/MenuItem";
+import type Project from "@lib/interfaces/Project";
 
 export const SITE_TITLE = "Usarral | Software Developer";
 export const NAME = "Carlos";
@@ -28,11 +26,11 @@ export const MENU_ITEMS: MenuItem[] = [
     Link: "/projects",
     id: "projects",
   },
-  //{
-  //  Page: "CV",
-  //  Link: "/cv",
-  //  id: "cv",
-  //},
+  {
+    Page: "CV",
+    Link: "/cv",
+    id: "cv",
+  },
   //{
   //  Page: "Contact",
   //  Link: "/contact",
@@ -99,7 +97,7 @@ export const PROJECTS: Project[] = [
 export const SOCIALS = [
   {
     title: "GitHub",
-    link: "httsp://github.com/usarral",
+    link: "https://github.com/usarral",
     icon: "github",
   },
   {
@@ -108,13 +106,62 @@ export const SOCIALS = [
     icon: "linkedin",
   },
   {
-    title: "Twitter",
-    link: "https://twitter.com/usarral",
-    icon: "twitter",
-  },
-  {
     title: "Email",
     link: "mailto:cv@usarral.com",
     icon: "mail",
+  },
+];
+export const WORK_EXPERIENCE = [
+  {
+    title: "Fullstack Java Developer",
+    subtitle: "July 2023 - Present at Minsait (Indra), [Remote, Spain]",
+    description: `
+Initially, I worked with Java 7, Java Servlets, and JSP, deploying applications on Tomcat 7 and 9, where I maintained and developed new features for a government client's customer culture project.`,
+    description2: `Currently, I am working with Java 8, Spring Boot, and Angular 14(TypeScript) deploying with Jenkins, Openshift and Docker, developing microservices for a government client's agriculture project.`,
+    description3: `On both projects I have worked with technologies like Git, Jira, Confluence, Oracle Database, and more.`,
+  },
+  {
+    title: "SAP ABAP4 Developer",
+    subtitle: "April 2023 - June 2024 at Minsait (Indra), [Remote, Spain]",
+    description: `
+    I worked with SAP ABAP4, developing new features and fixing bugs for a hotel chain project. I also worked with SAP UI5, developing new features for the same project.`,
+  },
+  {
+    title: "Web Developer",
+    subtitle:
+      "September 2018 - November 2022 at Ilusionista Pedro III, [Tudela, Spain]",
+    description: `
+    I developed and maintained the website of the magician Pedro III, where I implemented new features, fixed bugs, and more.`,
+    description2: `I also developed internal tools for the company.`,
+  },
+  {
+    title: "Application Developer",
+    subtitle: "March 2021 - June 2021 at Valortic, [Cintruénigo, Spain]",
+    description: `
+    I worked with the Power Platform environment, developing utilities, creating flows with Power Automate, R&D of proof of concepts with Power Apps, and more.`,
+    description2: `I also developed utilities such as internal APIs, database management pages, etc., based on Office 365, SharePoint...`,
+  },
+];
+export const EDUCATION = [
+  {
+    title: "Higher Technician in Web Application Development",
+    subtitle: "2021 to 2023 at CPIFP Los Enlaces, Zaragoza, Spain",
+  },
+  {
+    title: "Technician in Microcomputer Systems and Networks",
+    subtitle: "2019 to 2021 at CIP ETI Tudela, Tudela, Spain",
+  },
+];
+export const FEATURED = [
+  {
+    title: "NodeBot",
+    link: "https://github.com/LyricalString/Node-Discord-Bot",
+    description:
+      "NodeBot was a Discord bot that allows you to play music, moderate your server, and more. It was used in more than 27.000 servers and had more than 3M users.",
+  },
+  {
+    title: "AragonSkills",
+    link: "https://skills.aragon.es/",
+    description: `I participated in the AragonSkills 2023 competition representing CPIFP Los Enlaces in the Web Development category. I got the 3rd place in the competition.`,
   },
 ];
---
+++ src/lib/interfaces/Education.ts
@@ -0,0 +1,4 @@
+export default interface Education {
+  title: string;
+  subtitle: string;
+}
