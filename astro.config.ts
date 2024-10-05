25c442d15cd09b4298b39d2eed349cf0afa420c5
Carlos <41452910+usarral@users.noreply.github.com>
2024-10-05 14:04:35 +0200

    build: set output to server using vercel

--- astro.config.ts
+++ astro.config.ts
@@ -3,9 +3,15 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import { SITE_URL } from './src/data/config';
+import vercel from "@astrojs/vercel/serverless";
+
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercel({
+    includeFiles: ["./public/fonts/inter.ttf"],
+  }),
   site: SITE_URL,
   integrations: [mdx(), sitemap(), tailwind()]
 });
